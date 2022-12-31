ece11934617e601163aa6af55c419b528244d14e
Nisha Chandramoorthy <nishac@mit.edu>
2022-12-30 21:28:51 -0500

    solve newton step

--- newton.jl
+++ newton.jl
@@ -1,74 +1,74 @@
 using Test
 using LinearAlgebra
-function target_score(x, m, v, w, k)
-	prob = mixture_prob(x, m, v, w, k)
+function target_score(x, m, s, w, k)
+	prob = mixture_prob(x, m, s, w, k)
 	score = 0.0
 	for n = 1:k
-			score -= w[n]*gaussian(x,m[n],v[n])*(x-m[n])/v[n]/v[n]
+			score -= w[n]*gaussian(x,m[n],s[n])*(x-m[n])/s[n]/s[n]
 	end
 	score = 1/prob*score
 end
-function gaussian(x, m, v)
-	prob = (x-m)^2/2/v/v
+function gaussian(x, m, s)
+	prob = (x-m)^2/2/s/s
 	prob = exp(-prob)
-	return prob/sqrt(2*π)/v
+	return prob/sqrt(2*π)/s
 end
-function mixture_prob(x, m, v, w, k)
+function mixture_prob(x, m, s, w, k)
 	prob = 0
 	for n = 1:k
-		prob += w[n]*gaussian(x, m[n], v[n])
+		prob += w[n]*gaussian(x, m[n], s[n])
 	end
 	return prob
 end
-function source_score(x, m, v)
-	return -(x-m)/v/v
+function source_score(x, m, s)
+	return -(x-m)/s/s
 end
-function dtarget_score(x, m, v, w, k)
-	prob = mixture_prob(x,m,v,w,k)
-	score = target_score(x,m,v,w,k)
+function dtarget_score(x, m, s, w, k)
+	prob = mixture_prob(x,m,s,w,k)
+	score = target_score(x,m,s,w,k)
 	dscore = 0.0
 	for n = 1:k
-			dscore += gaussian(x, m[n], v[n])*w[n]*((x-m[n])^2/v[n]^4 - 1/v[n]^2)
+			dscore += gaussian(x, m[n], s[n])*w[n]*((x-m[n])^2/s[n]^4 - 1/s[n]^2)
 	end
 	dscore /= prob
 	dscore -= score*score
 	return dscore
 end
-function test_target_score(x,m,v,w,k)
+function test_target_score(x,m,s,w,k)
 	eps = 1.e-5
-	score = target_score(x,m,v,w,k)
-	score_fd = (log(mixture_prob(x+eps,m,v,w,k)) - log(mixture_prob(x-eps,m,v,w,k)))/(2*eps)
+	score = target_score(x,m,s,w,k)
+	score_fd = (log(mixture_prob(x+eps,m,s,w,k)) - log(mixture_prob(x-eps,m,s,w,k)))/(2*eps)
 	@show score, score_fd
 	@test score≈score_fd atol=1.e-6
 end
-function test_dtarget_score(x,m,v,w,k)
+function test_dtarget_score(x,m,s,w,k)
 	eps = 1.e-5
-	score1 = target_score(x+eps,m,v,w,k)
-	score2 = target_score(x-eps,m,v,w,k)
+	score1 = target_score(x+eps,m,s,w,k)
+	score2 = target_score(x-eps,m,s,w,k)
 	dscore_fd = (score1 - score2)/(2*eps)
-	dscore= dtarget_score(x,m,v,w,k)
+	dscore= dtarget_score(x,m,s,w,k)
 	@show "d scores from fd, analytical", dscore_fd, dscore
 	@test dscore_fd ≈ dscore atol=1.e-8
 end
-function solve_newton_step(N,r,mref,vref,m,v,w,k)
-	x_gr = LinRange(-r,r,N+1)
+function solve_newton_step(N,r,mref,sref,m,s,w,k)
+	x_gr = Array(LinRange(-r,r,N+1))
 	dx = x_gr[2] - x_gr[1]
-	x_gr = x_gr[1:N]
-	x_gr .+= dx/2
+	x = x_gr[2:N-1]
+	x .+= dx/2
 
 	dxinv = 1/dx
 	dx2inv = dxinv*dxinv
 
-	A = (-2.0*dx2inv)*I(N-2)
+	A = zeros(N-2,N-2)	
 	b = zeros(N-2)
-	x = x_gr[2:N-1]
+	
 	for n = 1:N-2
-		p = source_score(x[n], mref, vref)
-		q = target_score(x[n],m,v,w,k)
-		dq = dtarget_score(x[n],m,v,w,k)
+		p = source_score(x[n], mref, sref)
+		q = target_score(x[n],m,s,w,k)
+		dq = dtarget_score(x[n],m,s,w,k)
 
 		b[n] = p - q
-		if n < N-1
+		if n < N-2
 			A[n, n+1] += dx2inv
 			A[n, n+1] += p*dxinv/2
 		end
@@ -76,7 +76,8 @@ function solve_newton_step(N,r,mref,vref,m,v,w,k)
 			A[n, n-1] += dx2inv
 			A[n, n-1] += -p*dxinv/2
 		end
-		A[n, n] += dq
+		A[n, n] = dq -2.0*dx2inv
+
 	end
 	vint = A\b
 	v = zeros(N)
@@ -87,9 +88,10 @@ end
 w1, w2 = 0.5, 0.4
 w3 = 1 - (w1 + w2)
 m1, m2, m3 = 0.0, 1.0, 2.0
-v1, v2, v3 = 1.0, 0.5, 2.0
+s1, s2, s3 = 1.0, 0.5, 2.0
 w = [w1, w2, w3]
 m = [m1, m2, m3]
-v = [v1, v2, v3]
+s = [s1, s2, s3]
 x = rand()
+
 
